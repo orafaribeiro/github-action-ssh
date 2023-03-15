9cb311cecbd4f517de83205bc7fa4cbf97bea802
Anthony Rafael <anthonyribeiro1910@gmail.com>
2023-03-14 21:15:32 -0300

    ok

--- src/index.ts
+++ src/index.ts
@@ -54,7 +54,7 @@ async function connect(
     };
     if (privateKey) {
       console.log('using provided private key');
-      config.privateKey = privateKey;
+      config.privateKey = Buffer.from(privateKey);
     }
     await ssh.connect(config);
     console.log(`🤝 Connected to ${host}.`);
